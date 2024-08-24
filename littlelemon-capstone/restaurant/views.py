f4fb184e2a3eda96d30bcb6f46ff4c9bc3ea1cc1
Kirk Clarke <kclarke.design@gmail.com>
2024-08-24 13:17:53 -0400

    set up the Menu API for the Restaurant app

--- littlelemon-capstone/restaurant/views.py
+++ littlelemon-capstone/restaurant/views.py
@@ -1,7 +1,21 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Menu, Booking
+from .serializers import MenuSerializer, BookingSerializer
 
 # Create your views here.
+
+class MenuItemsView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+    
+    # def post(self, request, *args, **kwargs):
+    #     return Response({'message': ''}, status='')
+    
+class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
 
 def index(request):
     return render(request, 'index.html', {})
